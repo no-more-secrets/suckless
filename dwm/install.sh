5e4865e5f66d2dddf09312df560b2ec399802cc5
David P. Sicilia <dpsicilia@gmail.com>
2021-07-11 13:25:20 -0400

    Fix some path issues.

--- dwm/install.sh
+++ dwm/install.sh
@@ -7,4 +7,4 @@ cd /usr/share/xsessions
 [[ -e dwm.desktop ]] && exit 0
 
 echo "You may be asked for sudo password to install dwm.desktop file..."
-sudo ln -s /home/dsicilia/dev/suckless/dwm/dwm.desktop dwm.desktop
+sudo ln -s ~/dev/suckless/dwm/dwm.desktop dwm.desktop
