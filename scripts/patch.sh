c95d6fda31fdf5598c4db072ed2d5b70641d1cf3
David P. Sicilia <dpsicilia@gmail.com>
2021-07-11 10:22:23 -0400

    Separate st patch into patches.

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -4,8 +4,17 @@
 set -e
 set -o pipefail
 
+c_norm="\033[00m"
+c_green="\033[32m"
+c_red="\033[31m"
+c_yellow="\033[93m"
+
+warn() {
+  echo -e "${c_yellow}WARNING${c_norm}: $@" 1>&2
+}
+
 die() {
-  echo "$@" 1>&2
+  echo -e "${c_red}error${c_norm}: $@" 1>&2
   exit 1
 }
 
@@ -32,7 +41,8 @@ set -e
 case $code in
   # Success error code means that files are the same, which they
   # should not be.
-  0) die "config.def.h and config.h are the same, no patching occurred." ;;
+  0) warn "config.def.h and config.h are the same, no patching occurred. " \
+          "This can happen the very first time a patch is created for a tool." ;;
   # diff succeeded and files are different.
   1) ;;
   2) die "failed to diff config.def.h and config.h." ;;
