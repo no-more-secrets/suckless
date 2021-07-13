fb765e2c86bcbf7f18e8e25522e6ff72f95ff2cb
David P. Sicilia <dpsicilia@gmail.com>
2021-07-13 08:39:35 -0400

    Add anysize patch to st.

--- st/patch.sh
+++ st/patch.sh
@@ -7,10 +7,12 @@ cd "$(readlink -f latest)"
 # Prepare the files (specific to st).
 git checkout -- \
   config.def.h  \
-  st.def.c
+  st.def.c      \
+  x.def.c
 
 cp config.def.h config.h
 cp st.def.c     st.c
+cp x.def.c      x.c
 
 cd -
 
